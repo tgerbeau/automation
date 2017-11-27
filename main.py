9fa88871652d1064d2090b3cb5186316727cb348
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2017-11-27 18:17:06 +0100

    add parameters condition

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 
+import sys
 import library
+import config
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox(executable_path='/home/tgerbeau/Documents/geckodriver')
-driver.get("https://ginco.naturefrance.fr/dailybuild")
+driver = library.setUp()
+print (len(sys.argv))
 
-assert "Plateforme GINCO Dailybuild" in driver.title
+#If any region is given in parameter, default region = dailybuild
+if len(sys.argv) == 1:
+    region = config.URL_PLATEFORM['default_region']
+else:
+    region = sys.argv[1]
+
+driver.get (config.URL_PLATEFORM ['base_url'] + region)
+#assert "Plateforme GINCO Dailybuild" in driver.title
 
 library.login(driver)
-library.logout(driver)
-driver.close()
+library.createDataSet(driver, region)
+#library.logout(driver)
+library.tearDown(driver)

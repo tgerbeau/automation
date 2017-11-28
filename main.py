d595fc5a29c34b7ccb5c08080aaea25874933c73
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2017-11-28 16:15:45 +0100

    add createDataSet + checkImport

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import config
 #from selenium.webdriver.common.keys import Keys
 
 driver = library.setUp()
-print (len(sys.argv))
 
 #If any region is given in parameter, default region = dailybuild
 if len(sys.argv) == 1:
     region = config.URL_PLATEFORM['default_region']
 else:
-    region = sys.argv[1]
+    region = sys.argv[1] + "/"
 
 driver.get (config.URL_PLATEFORM ['base_url'] + region)
 #assert "Plateforme GINCO Dailybuild" in driver.title
 
 library.login(driver)
 library.createDataSet(driver, region)
+library.checkImport(driver, region)
+
 #library.logout(driver)
 library.tearDown(driver)

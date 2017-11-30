bf40fb99f64c9f320993aae20abaadf0af78a319
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2017-11-30 17:47:31 +0100

    add config file

---
+++ config.py
@@ -0,0 +1,21 @@
+USER_CREDENTIALS = {
+    'username': '',
+    'password': ''
+}
+URL_PLATEFORM = {
+    'base_url' : 'https://ginco.naturefrance.fr/',
+    'default_region' : 'dailybuild/',
+    'create_dataset' : 'jdd/new',
+    'all_dataset' : 'jdd/all'
+}
+ID_METADATA = {
+    'id' : '4A9DDA1F-B6F8-3E13-EO53-2614A8C02B7C',
+    'id2' : '4A9DDA1F-B770-3E13-E053-2614A8C02B7C',
+    'id3' : '4A9DDA1F-B6EF-3E13-E053-2614A8C02B7C'
+}
+CSV_FILENAME = {
+    'csv_filename' : '/dee_test_mnhn.csv',
+}
+SRID = {
+    'wgs84' : '4326'
+}

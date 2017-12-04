2f2ab537612eba4c0e9973ff0d530ffa2b06cec2
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2017-12-04 17:32:55 +0100

    add removeDataSet

--- main.py
+++ main.py
@@ -20,9 +20,8 @@ id_metadata = config.ID_METADATA['id3']
 # Built url to access to all dataset list
 url_all_dataset = config.URL_PLATEFORM ['base_url'] + region + config.URL_PLATEFORM ['all_dataset']
 
-
 library.login(driver)
-# Clean previous import with the same metadata id
+# Clean previous import(s) using the same metadata id
 library.removeDataSet(driver, url_all_dataset, id_metadata)
 
 # A valid data model is required before creating a new data set
@@ -30,6 +29,9 @@ library.createDataSet(driver, region, id_metadata)
 
 # Check that the nblines imported are not null
 library.checkImport(driver, url_all_dataset)
+
+# Clean previous import(s) using the same metadata id
+library.removeDataSet(driver, url_all_dataset, id_metadata)
 
 # library.logout(driver)
 library.tearDown(driver)

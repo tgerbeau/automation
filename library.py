8498a0cb3ee5873a69d1bac6d55cebcb51980c41
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2017-12-12 09:48:21 +0100

    improve removeDataSet function

--- library.py
+++ library.py
@@ -81,20 +81,36 @@ def checkImport (driver, url_all_dataset) :
 def removeDataSet (driver, url_all_dataset , id_metadata) :
     # Go on jdd/all page
     driver.get (url_all_dataset)
-    driver.get (url_all_dataset)
 
     # Use searching bar with given id_metadata
-    search = driver.find_element_by_css_selector('input.form-control')
-    search.send_keys(id_metadata)
+    #search = driver.find_element_by_css_selector('input.form-control')
+    #search.send_keys(id_metadata)
 
     # Get all the metadata elements in page
     elements = driver.find_elements_by_css_selector("span.text-muted.longtext")
-    # //*[@id="integration-cancel-link-131"]/span
-    # //*[@id="jddTable"]/tbody/tr[1]
-    # Check if id_metadata is on the list of elements
+    i=1
     for ii in elements:
-        print (ii.text)
+        # Check if id_metadata is on the list of elements
         if (id_metadata == ii.text):
-            print ("Same metadata found")
             # Clean all imports
             empty_imports = False
+            while empty_imports is False:
+                try:
+                    str_xpath = "//*[@id=\"jddTable\"]/tbody/tr[" + str (i) + "]/td[5]/div/div/a[2]"
+                    btn_cancel_submission = driver.find_element_by_xpath(str_xpath)
+                    btn_cancel_submission.click()
+                    # Catch the popup alert Yes button
+                    alert = driver.switch_to.alert.accept()
+                    # Page reloading
+                    # driver.get (url_all_dataset)
+                except:
+                    empty_imports = True
+                    # Clean the general data set (main delete button)
+                    str_xpath_remove = "//*[@id=\"jddTable\"]/tbody/tr[" + str (i) + "]/td[1]/button/span"
+                    btn_cancel_submission = driver.find_element_by_xpath(str_xpath_remove)
+                    btn_cancel_submission.click()
+                    # Catch the popup alert, click on "Continuer" button
+                    btn_continuer = driver.find_element_by_link_text('Continuer')
+                    btn_continuer.click()
+                    print (">> removeDataSet(): Previous imports have been correctly removed.")
+        i= i + 1
